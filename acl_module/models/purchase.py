2dc383e65edf9a4127e6e7f9d925cda36797ba5e
unknown <louis@advences.com>
2022-05-17 11:05:15 +0300

    envoie mail fournisseur - lors de confirmation de facture de vente

--- acl_module/models/purchase.py
+++ acl_module/models/purchase.py
@@ -8,43 +8,43 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def send_mail_confirmation_purchase(self):
-        if self.state == 'purchase':
-            subject = 'Demande de confirmation'
-            if self.x_customer != None:
-                subject = subject + ' - ' + str(self.x_customer)
-            if self.x_first_service_date != None:
-                subject = subject + ' - ' + str(self.x_first_service_date)
-            if self.name != None:
-                subject = subject + ' - ' + str(self.name)
+        #if self.state == 'purchase':
+        subject = 'Demande de confirmation'
+        if self.x_customer != None:
+            subject = subject + ' - ' + str(self.x_customer)
+        if self.x_first_service_date != None:
+            subject = subject + ' - ' + str(self.x_first_service_date)
+        if self.name != None:
+            subject = subject + ' - ' + str(self.name)
 
-            lines = self.env['purchase.order.line'].sudo().search([("order_id", "=", self._origin.id)])
-            content = ""
-            for line in lines:
-                content = content + """<tr t-att-class="bg-200 font-weight-bold o_line_section"><td name="td_name" style="width:80%">""" + line.name + """</td><td name="td_quantity" class="text-right">""" + str(line.price_subtotal) + """ """ + line.currency_id.symbol + """</td></tr>"""
+        lines = self.env['purchase.order.line'].sudo().search([("order_id", "=", self._origin.id)])
+        content = ""
+        for line in lines:
+            content = content + """<tr t-att-class="bg-200 font-weight-bold o_line_section"><td name="td_name" style="width:80%">""" + line.name + """</td><td name="td_quantity" class="text-right">""" + str(line.price_subtotal) + """ """ + line.currency_id.symbol + """</td></tr>"""
 
-            body_html = """<p>Cher partenaire,</p></br>
-                <p>Merci de confirmer la commande suivante :</p></br>
-                <table class="table table-sm o_main_table" style="width:70%">
-                    <thead style="display: table-row-group">
-                        <tr>
-                            <th name="th_description" class="text-left">Prestation(s)</th>
-                            <th name="th_quantity" class="text-right">Prix</th>
-                        </tr>
-                    </thead>
-                    <tbody class="sale_tbody">""" +  content + """</tbody>
-                </table>
-                <p>Cordialement,</p>
-                <p>Equipe Authentik Caledonia</p>"""
+        body_html = """<p>Cher partenaire,</p></br>
+            <p>Merci de confirmer la commande suivante :</p></br>
+            <table class="table table-sm o_main_table" style="width:70%">
+                <thead style="display: table-row-group">
+                    <tr>
+                        <th name="th_description" class="text-left">Prestation(s)</th>
+                        <th name="th_quantity" class="text-right">Prix</th>
+                    </tr>
+                </thead>
+                <tbody class="sale_tbody">""" +  content + """</tbody>
+            </table>
+            <p>Cordialement,</p>
+            <p>Equipe Authentik Caledonia</p>"""
 
-            template_data = {
-                'subject': subject,
-                'body_html': body_html,
-                'email_from': 'authentikcaledonia@gmail.com',
-                'email_to': self.partner_id.email
-            }
-            template_obj = self.env['mail.mail']
-            template_id = template_obj.create(template_data)
-            template_obj.send(template_id)
+        template_data = {
+            'subject': subject,
+            'body_html': body_html,
+            'email_from': 'authentikcaledonia@gmail.com',
+            'email_to': self.partner_id.email
+        }
+        template_obj = self.env['mail.mail']
+        template_id = template_obj.create(template_data)
+        template_obj.send(template_id)
 
     """def button_confirm(self):
         for order in self:
